9025aa4ad45efa75ee50e12cb47f80287075f385
Thisal Kenula <thisalkenula@outlook.com>
2025-02-03 12:05:07 +0530

    Implement Radix Sort

--- Python/radix_sort.py
+++ Python/radix_sort.py
@@ -1,13 +1,32 @@
 def radix_sort(arr: list):
     '''
+    Sorts the given list of non-negaitve numbers in place using Radix Sort algorithm
     arr: list of non-negative numbers
+    return: None
     '''
     e = 1
-    n = max(myArray)
-    while n//e > 0:
-        for n in arr:
-            # Continue from here
-            pass
+    max_n = max(arr)
+
+    # Loop until all the digits of the maximum number is considered
+    while max_n // e > 0:
+        digi_grp = [[] for i in range(10)]  # Create 10 empty lists for each digit group
+
+        # Add the numbers to the respective digit group
+        for num in arr:
+            digi_grp[(num//e)%10].append(num)
+        
+        arr.clear() # Clear the array before adding the sorted elements
+        # arr = [] will not work as it will create a new list and the reference will be lost
+
+        for i in range(10):
+            for j in digi_grp[i]:
+                arr.append(j)
+
+        e *= 10 # Move to the next digit
+    
 
 
 myArray = [170, 45, 75, 90, 802, 24, 2, 66]
+
+radix_sort(myArray)
+print(myArray)
