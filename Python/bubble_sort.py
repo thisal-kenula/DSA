21099ceff8d609a20c1445277daa09344ad208d5
Thisal Kenula <thisalkenula@outlook.com>
2025-02-14 19:05:54 +0530

    Complete bubble sort in Python

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -1,10 +1,13 @@
 def bubble_sort(arr: list):
     n = len(arr)
+    # Traverse through all array elements - 1 
+    # (last element is automatically sorted)
     for i in range(n - 1):
-        for j in range(i, n - 1):
+        # Traverse the unsorted part of the array
+        for j in range(n - i - 1):
+            # Swap
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                # CONTINUE HERE
 
 myArray = [170, 45, 75, 90, 802, 24, 2, 66]
 bubble_sort(myArray)
