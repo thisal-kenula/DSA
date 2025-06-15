6ae9050c286367018cf35b308bd1dc80b2df1bd4
Thisal Kenula <thisalkenula@outlook.com>
2025-06-15 19:03:50 +0530

    Add insertNode to linkedListOperations.c

--- C/linkedListOperations.c
+++ C/linkedListOperations.c
@@ -1,9 +1,11 @@
 /*
 * Linked list operations on a singly linked list
+* Not tested to handle edge cases
 * Includes:
 *   - Traversal
 *   - Find lowest value
-*   - 
+*   - Delete a node
+    - Insert a node
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -77,7 +79,7 @@ int findLowestVal(Node* node) {
  * @returns Pointer to new head of the linked list
  */
 Node* deleteNode(Node* head, Node* nodeToDelete) {
-    // If the code to delete is a head
+    // If the node to delete is a head
     if (head == nodeToDelete) {
         Node* newHead = head->next;
         free(head);
@@ -100,6 +102,42 @@ Node* deleteNode(Node* head, Node* nodeToDelete) {
     free(nodeToDelete);
 
     return head;
+
+    
+}
+
+/**
+ * Insert new node
+ * 
+ * Will raise an error if `at` is larger than the available nodes - 1
+ * 
+ * @param head Pointer to head of the linked list
+ * @param node Pointer to new node
+ * @param at Position to insert (first = 0)
+ * 
+ * @returns Updated head (pointer)
+ */
+Node* insertNode(Node* head, Node* nodeToInsert, int at) {
+    // Insert at the beginning
+    if (at == 0) {
+        nodeToInsert->next = head;
+        return nodeToInsert;
+    }
+    
+    Node* currentNode = head;
+    for (int i = 0; i < at - 1; ++i) {
+        // If `at` is larger than available nodes - 1
+        currentNode = currentNode->next;
+        if (!currentNode) {
+            printf("Linked list has no such position\n");
+            exit(1);
+        }
+    }
+
+    nodeToInsert->next = currentNode->next;
+    currentNode->next = nodeToInsert;
+
+    return head;
 }
 
 // Testing
@@ -113,23 +151,33 @@ int main() {
     node2->next = node3;
     node3->next = node4;
 
-    printf("Linked list: ")''
+    // Test Traverse
+    printf("Linked list: ");
     traverse(node1);
 
+    // Test Find lowest value
     printf("Lowest value: ");
     int i = findLowestVal(node1);
     printf("%d\n", i);
 
+    // Test deleting node
     Node* newHeadNode = deleteNode(node1, node3);
-    printf("Linked list after deleting node3");
+    printf("Linked list after deleting node3: ");
     traverse(newHeadNode);
 
-    // This works for this specific case. But it's reecommended.
+    // Test inserting a node
+    Node* newNode = createNode(14);
+    Node* newHeadNode2 = insertNode(newHeadNode, newNode, 3);
+    printf("Linked list after inserting node(14): ");
+    traverse(newHeadNode2);
+
+    // This works for this specific case. But it's recommended.
     // Use a dedicated function to free the list.
     free(node1);
     free(node2);
     // free(node3); // This is already freed when deleting the node
     free(node4);
+    free(newNode);
 
     return 0;
 }
